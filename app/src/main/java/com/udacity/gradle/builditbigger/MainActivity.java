dfda741ebbcf10d17be9a02e0c162e4c4ab4b89e
Jaren831 <jaren.lynchmobb@gmail.com>
2017-01-23 23:07:26 -0800

    Adds GCE guidance

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
         setContentView(R.layout.activity_main);
     }
 
