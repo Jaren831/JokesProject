845aa27b3fd2901bd69edc5a70214c5e5f26e476
Jaren831 <jaren.lynchmobb@gmail.com>
2017-02-03 01:49:43 -0800

    Adds more jokes to jokelibrary

--- JokeLibrary/src/main/java/com/example/JokeProvider.java
+++ JokeLibrary/src/main/java/com/example/JokeProvider.java
@@ -5,7 +5,9 @@ import java.util.Random;
 public class JokeProvider {
 
     private static final String[] jokes = {
-            "This is totally a funny joke."
+            "This is totally a funny joke.",
+            "Blah, blah, blah",
+            "Ha, ha, ha, ha",
     };
     public static String getJoke(){
         int random = new Random().nextInt(jokes.length);
